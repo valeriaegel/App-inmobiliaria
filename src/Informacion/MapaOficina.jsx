ddf5d48e6dafcc75601dfe4593cefb7afc5ba468
Valeria Egel <valeriaegel@gmail.com>
2025-12-11 00:12:39 -0300

    Mapa funcionando

--- src/Informacion/MapaOficina.jsx
+++ src/Informacion/MapaOficina.jsx
@@ -2,22 +2,22 @@ import {AdvancedMarker, APIProvider, Map} from '@vis.gl/react-google-maps';
 
 //iNFORMACION SOBRE LIBRERIA  https://github.com/visgl/react-google-maps 
 
-// const position = {
-//     lat: -32.4837462, 
-//     lng: -58.2315257  
-// };
+ const position = {
+     lat: -32.4837462, 
+     lng: -58.2315257  
+ };
 
 function MapaOficina() {
     const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY; 
 
     return (
        
-    //        <APIProvider apiKey={API_KEY}>
-    //   <Map defaultCenter={position} defaultZoom={15} mapId="UBICACION_OFICINA">
-    //     <AdvancedMarker position={position} />
-    //   </Map>
-    // </APIProvider>
-    <div></div>
+            <APIProvider apiKey={API_KEY}>
+       <Map defaultCenter={position} defaultZoom={15} mapId="UBICACION_OFICINA">
+         <AdvancedMarker position={position} />
+       </Map>
+     </APIProvider>
+   
         
     );
 }
